e8de85bddafcab70d208ab6d8bae7eb4ac33c03e
Dan Isaev <dkikrlv2@mail.ru>
2021-09-16 04:00:27 +0300

    Cleaned backend from special logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,47 +4,54 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 mongoose.connect('mongodb://localhost:27017/local');
-const schema = new mongoose.Schema({ spendings: Array, incomes: Array, after: Object, previous: mongoose.ObjectId }, );
+
+const schema = new mongoose.Schema({
+  spendings: Array,
+  incomes: Array,
+  after: Object,
+  before: { type: Object, required: false },
+  previous: mongoose.ObjectId
+});
+
 const PeriodModel = mongoose.model('periods', schema);
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({ origin: 'http://localhost:3000' }));
 
+app.use((__, res, next) => {
+  res.header('Content-Type', 'application/json');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Root OK!');
 });
 
-app.get('/periods', async (req, res) => {
+app.get('/periods', async (__, res) => {
+  const periodsDocs = await PeriodModel.find();
   
-  const result = await PeriodModel.find();
-  console.log('result is', result);
+  res.send(periodsDocs.map(item => _.omit(item.toJSON(), '__v')));
+});
 
-  res.header('Content-Type', 'application/json');
-  res.send(result.map(item => _.omit(item, '__v')));
+app.get('/periods/:id', async (req, res) => {
+  const { id } = req.params;
+  const period = await PeriodModel.findOne({ _id: id });
+
+
+  res.send(_.omit(period.toJSON(), '__v'));
 });
 
 app.post('/periods', async (req, res) => {
-  const allPeriods = await PeriodModel.find();
-
-  const prevs = allPeriods.map(period => period.previous ? period.previous.toString() : null);
-  console.log('prevs', prevs);
-  const lastPeriod = allPeriods.find(period => !prevs.includes(period._id.toString()));
-  console.log('body is', req.body, typeof req.body);
-  const newPeriod = new PeriodModel({ ...req.body, previous: lastPeriod ? lastPeriod._id : null });
+  const newPeriod = new PeriodModel(req.body);
 
   const savedPeriod = await newPeriod.save();
-  const previousPeriod = await PeriodModel.findById(savedPeriod.previous);
-  console.log({ savedPeriod: savedPeriod.toJSON(), previousPeriod: previousPeriod.toJSON() });
-
-  res.send(_.omit({ ...savedPeriod.toJSON(), before: previousPeriod.toJSON().after }, '__v'));
-
+  
+  res.send(_.omit(savedPeriod.toJSON(), '__v'));
 });
 
 app.patch('/periods/:id', async (req, res) => {
-  console.log('req.params are', req.params);
   const result = await PeriodModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, req.body);
-  console.log('result is', result);
 
   res.send(_.pick(result, ['n', 'nModified']));
 });
